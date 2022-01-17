7d9d00439b88904888dade553fce8fbc6654c059
starry <starry90@qq.com>
2022-01-17 14:04:41 +0800

    解决跨域问题（blocked by CORS policy）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,9 @@
 
     <input class="search-input width100p">
 
+    <button @click="testAxios" class="button" type="button">测试</button>
+    <button @click="getAllNotice" class="button" type="button">获取预告</button>
+
   </div>
 </template>
 
@@ -12,7 +15,26 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  methods: {
+    testAxios() {
+      // 由于 main.js 里全局定义的 axios,此处直接使用 $axios 即可。
+      // 经过 vue.config.js 配置文件的代理设置，会自动转为 https://www.baidu.com/，从而解决跨域问题
+      this.$axios.get('/api').then(response => {
+        if (response.data) {
+          console.log(response.data)
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    getAllNotice() {
+      console.log("获取预告")
+    }
   }
+
 }
 </script>
 
@@ -25,6 +47,11 @@ export default {
 
 .search-input {
   height: 35px;
+}
+
+.button {
+  height: 35px;
+  margin: 10px;
 }
 
 </style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            //跨域问题
+            //Access to XMLHttpRequest at 'https://www.baidu.com/' from origin 'http://localhost:8080' has been
+            // blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+            '/api': {
+                // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
+                target: 'https://www.baidu.com/',
+                // 允许跨域
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+}
